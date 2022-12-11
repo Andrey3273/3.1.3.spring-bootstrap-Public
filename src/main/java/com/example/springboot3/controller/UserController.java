ca921abc79daad61611aff940f77dd40aebc4cd7
Andrey <andry.ivankov@ya.ru>
2022-12-11 13:31:17 +0300

    .

--- src/main/java/com/example/springboot3/controller/UserController.java
+++ src/main/java/com/example/springboot3/controller/UserController.java
@@ -2,7 +2,6 @@ package com.example.springboot3.controller;
 
 import com.example.springboot3.service.RoleService;
 import com.example.springboot3.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,59 +15,43 @@ import java.util.Set;
 @Controller
 @RequestMapping(value = "/")
 public class UserController {
+
     private final UserService userService;
     private final RoleService roleService;
-    @Autowired
-    public UserController(RoleService roleService, UserService userService) {
-        this.roleService = roleService;
+
+    public UserController(UserService userService, RoleService roleService) {
         this.userService = userService;
+        this.roleService = roleService;
     }
+
     @GetMapping(value = "/user")
-    public String userInfo(@AuthenticationPrincipal User user, Model model){
+    public String userInfo(@AuthenticationPrincipal User user, Model model) {
         model.addAttribute("user", user);
         model.addAttribute("roles", user.getRoles());
         return "userpage";
     }
-    @GetMapping(value = "/admin")
-    public String listUsers(Model model) {
-        model.addAttribute("allUsers", userService.getAllUsers());
-        return "all-user";
-    }
-    @GetMapping(value = "/admin/new")
-    public String newUser(Model model) {
-        model.addAttribute("user", new User());
-        model.addAttribute("roles", roleService.getAllRoles());
-        return "info";
-    }
-    @PostMapping(value = "/admin/add-user")
-    public String addUser(@ModelAttribute User user, @RequestParam(value = "checkBoxRoles") String[] checkBoxRoles) {
-        Set<Role> roleSet = new HashSet<>();
-        roleService.checkBoxRole(checkBoxRoles);
-        user.setRoles(roleSet);
-        userService.addUser(user);
-        return "redirect:/admin";
-    }
+
     @GetMapping(value = "/{id}/edit")
     public String editUserForm(@PathVariable("id") long id, Model model) {
         model.addAttribute("user", userService.getUserById(id));
         model.addAttribute("roles", roleService.getAllRoles());
         return "edit";
     }
+
     @PatchMapping(value = "/{id}")
     public String editUser(@ModelAttribute User user, @RequestParam(value = "checkBoxRoles") String[] checkBoxRoles) {
         Set<Role> roleSet = new HashSet<>();
-        roleService.checkBoxRole(checkBoxRoles);
+        for (String roles : checkBoxRoles) {
+            roleSet.add(roleService.getRoleByName(roles));
+        }
         user.setRoles(roleSet);
         userService.updateUser(user);
         return "redirect:/admin";
     }
-    @DeleteMapping(value = "/remove/{id}")
+
+    @PostMapping(value = "/remove/{id}")
     public String removeUser(@PathVariable("id") long id) {
         userService.removeUserById(id);
         return "redirect:/admin";
-    }
-    @GetMapping(value = "/login")
-    public String login(){
-        return "login";
     }
 }
